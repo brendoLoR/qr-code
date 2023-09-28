50d824b27f1ea5debc2c44ce96ab0d8941e080b7
Brendo Silva <brendoja@gmail.com>
2023-09-28 18:06:35 -0300

    add build

---
+++ public/build/assets/InputError-fc9e566d.js
@@ -0,0 +1,1 @@
+import{i as s,s as t,o as a,f as r,b as o,t as c}from"./app-e80bbe94.js";const n={class:"text-sm text-red-600 dark:text-red-400"},p={__name:"InputError",props:["message"],setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
